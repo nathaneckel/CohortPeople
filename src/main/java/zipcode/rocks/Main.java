15f1cb74d8c82c58b88efb95794c4d2bfa2dffe5
nathaneckel <nathan@patientpaperwork.com>
2024-03-03 16:02:50 -0500

    draft1 please see comments and advise thx

--- src/main/java/zipcode/rocks/Main.java
+++ src/main/java/zipcode/rocks/Main.java
@@ -12,7 +12,7 @@ public class Main {
     private static final String COMMA_DELIMITER = ",";
 
     public static void main(String[] args) {
-        String student_source = "FakeData.csv";
+        String student_source = "cohortData.csv";
 
         // Load data file into an ArrayList<Person> of Person objects.
         Main programObject = new Main();
@@ -44,8 +44,210 @@ public class Main {
     private Person getRecordFromLine(String line) {
         // take the String of lastname,firstname and create a new Person object with it.
         String[] values = line.split(COMMA_DELIMITER);
-        Person newPerson = new Person(values[0], values[1]);
+        Person newPerson = new Person(values[0], values[1], values[2], values[3], values[4], values[5]);
         return newPerson;
     }
 
+
+    private List handWrittenLoadOfData() {
+        ArrayList<Person> students = new ArrayList<>();
+
+        Person newPerson = new Person("Abukar", "Abukar");
+        newPerson.setGithubHandle("abukarabukar");
+        newPerson.setFavoriteColor("yellow");
+        newPerson.setBirthMonth("April");
+        newPerson.setLikePets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Chaz", "Adams");
+        newPerson.setGithubHandle("chaz_adams");
+        newPerson.setFavoriteColor("blue");
+        newPerson.setBirthMonth("February");
+        newPerson.setLikePets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Asan", "Ahmadli,");
+        newPerson.setGithubHandle("asanahamdli");
+        newPerson.setFavoriteColor("blue");
+        newPerson.setBirthMonth("December");
+        newPerson.setHasPets("false");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Jenn", "Azer");
+        newPerson.setGithubHandle("cupofjavajenny");
+        newPerson.setFavoriteColor("blue");
+        newPerson.setBirthMonth("November");
+        newPerson.setHasPets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Ali", "Bangash");
+        newPerson.setGithubHandle("ali-b14");
+        newPerson.setFavoriteColor("green");
+        newPerson.setBirthMonth("March");
+        newPerson.setHasPets("false");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Jake", "Brennan");
+        newPerson.setGithubHandle("jake-brennan714");
+        newPerson.setFavoriteColor("purple");
+        newPerson.setBirthMonth("April");
+        newPerson.setHasPets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Trey", "Bruton");
+        newPerson.setGithubHandle("tb0902_");
+        newPerson.setFavoriteColor("orange");
+        newPerson.setBirthMonth("September");
+        newPerson.setHasPets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Danny", "Cao");
+        newPerson.setGithubHandle("dannycao1997");
+        newPerson.setFavoriteColor("September");
+        newPerson.setBirthMonth("March");
+        newPerson.setHasPets("false");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Nathan", "Eckel");
+        newPerson.setGithubHandle("nathaneckel");
+        newPerson.setFavoriteColor("green");
+        newPerson.setBirthMonth("February");
+        newPerson.setHasPets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Jorris", "Ekoloko");
+        newPerson.setGithubHandle("reeseyverse");
+        newPerson.setFavoriteColor("black");
+        newPerson.setBirthMonth("August");
+        newPerson.setHasPets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Alice", "Gonzalez");
+        newPerson.setGithubHandle("NEEDGITHUB");
+        newPerson.setFavoriteColor("blue");
+        newPerson.setBirthMonth("May");
+        newPerson.setHasPets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Deepa", "Kakade");
+        newPerson.setGithubHandle("NEEDGITHUB");
+        newPerson.setFavoriteColor("pink");
+        newPerson.setBirthMonth("June");
+        newPerson.setHasPets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Mariyana", "Katzarova");
+        newPerson.setGithubHandle("mariyana-katz");
+        newPerson.setFavoriteColor("green");
+        newPerson.setBirthMonth("December");
+        newPerson.setHasPets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Lydia", "Konstanski");
+        newPerson.setGithubHandle("lydiastonekonstanski");
+        newPerson.setFavoriteColor("black");
+        newPerson.setBirthMonth("October");
+        newPerson.setHasPets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Matt", "Kramer");
+        newPerson.setGithubHandle("NEEDGITHUB");
+        newPerson.setFavoriteColor("green");
+        newPerson.setBirthMonth("January");
+        newPerson.setHasPets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Angelina", "Mathew");
+        newPerson.setGithubHandle("angelinamathew");
+        newPerson.setFavoriteColor("blue");
+        newPerson.setBirthMonth("May");
+        newPerson.setHasPets("false");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Chris", "McCall");
+        newPerson.setGithubHandle("christopher_mccall");
+        newPerson.setFavoriteColor("blue");
+        newPerson.setBirthMonth("May");
+        newPerson.setHasPets("false");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Nick", "McNamara");
+        newPerson.setGithubHandle("nicholasmacnamara");
+        newPerson.setFavoriteColor("green");
+        newPerson.setBirthMonth("January");
+        newPerson.setHasPets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Shaily", "Modi");
+        newPerson.setGithubHandle("*NEED");
+        newPerson.setFavoriteColor("*NEED");
+        newPerson.setBirthMonth("*NEED");
+        newPerson.setHasPets("*NEED");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Dan", "Moffett-Conaway");
+        newPerson.setGithubHandle("moffd234");
+        newPerson.setFavoriteColor("blue");
+        newPerson.setBirthMonth("June");
+        newPerson.setHasPets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Lavanya", "Palanikumar");
+        newPerson.setGithubHandle("lavanyapalanikumar93");
+        newPerson.setFavoriteColor("blue");
+        newPerson.setBirthMonth("October");
+        newPerson.setHasPets("false");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Anthony", "Pearson");
+        newPerson.setGithubHandle("anthonyp365");
+        newPerson.setFavoriteColor("red");
+        newPerson.setBirthMonth("July");
+        newPerson.setHasPets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Ethan", "Rieger");
+        newPerson.setGithubHandle("erieger11");
+        newPerson.setFavoriteColor("blue");
+        newPerson.setBirthMonth("May");
+        newPerson.setHasPets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Jorden", "Ross");
+        newPerson.setGithubHandle("jross911");
+        newPerson.setFavoriteColor("green");
+        newPerson.setBirthMonth("December");
+        newPerson.setHasPets("false");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Michael", "Scott");
+        newPerson.setGithubHandle("michaelscott44");
+        newPerson.setFavoriteColor("orange");
+        newPerson.setBirthMonth("May");
+        newPerson.setHasPets("true");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Diksha", "Sharma");
+        newPerson.setGithubHandle("dikshasharmaa");
+        newPerson.setFavoriteColor("blue");
+        newPerson.setBirthMonth("March");
+        newPerson.setHasPets("false");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Shijin", "Zeng");
+        newPerson.setGithubHandle("szeng89");
+        newPerson.setFavoriteColor("yellow");
+        newPerson.setBirthMonth("July");
+        newPerson.setHasPets("false");
+        record.add(newPerson);
+
+        Person newPerson = new Person("Andy", "Zheng");
+        newPerson.setGithubHandle("andyzheng679");
+        newPerson.setFavoriteColor("red");
+        newPerson.setBirthMonth("December");
+        newPerson.setHasPets("false");
+        record.add(newPerson);
+
+        return students;
+    }
 }
--- src/main/java/zipcode/rocks/Person.java
+++ src/main/java/zipcode/rocks/Person.java
@@ -1,14 +1,30 @@
 package zipcode.rocks;
 
+//While trying to follow the high level logic I have obviously created many dozens of errors.
+//Instead of trying to fix every single error immediately I an trying to replicate the logic on a high level.
+//My strong fear/aversion to fixing errors immediately - is that I tend to go on a rabbit trail
+//I want to build the frame of the code first, then attack the errors.
+//This may likely be completely counterintuitive to the coding process.
+//The more I focus on details/errors, the slower I tend to go and the more I zoom into the problem.
+//Please advise if I need to completely change the way that I approach coding.
+
 public class Person {
     private String firstname;
     private String lastname;
+    private String color;
+    private String month;
+    private String pet;
+    private String handle
 
-    public Person(String fname, String lname) {
+    //Looking for where / how to add the person
+    public Person(String fname, String lname, String color, String month, String pet, String handle) {
         this.firstname = fname;
         this.lastname = lname;
+        this.color = color;
+        this.month = month;
+        this.pet = pet;
+        this.handle = handle;
     }
-
 
     public String getFirstname() {
         return firstname;
@@ -27,6 +43,30 @@ public class Person {
     }
 
     public String toString() {
-        return this.lastname + ", " + this.firstname;
+        return this.lastname + ", " + this.firstname
+                + this.color + this.month + this.pet + this.handle;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+    public void setMonth(String month) {
+        this.month = month;
+    }
+    public String getPet() {
+        return pet;
+    }
+
+    public void setHandle(String handle) {
+        this.handle = handle;
+    }
+    public String getHandle() {
+        return handle;
     }
 }
